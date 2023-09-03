760c5ef1ece6b5b3bc98b8d61154c4101f3a584d
Lucas Manoguerra <kiu.manoguerra@gmail.com>
2023-09-03 18:58:25 -0300

    feat: agregar SetupPage para configuración inicial
    Este commit agrega SetupPage, una página que maneja la configuración inicial del usuario. Verifica si el usuario ya está configurado en un servidor y muestra una modal inicial si no lo está.

---
+++ src/app/(setup)/page.tsx
@@ -0,0 +1,31 @@
+import { InitialModal } from "@/components/modals/initial-modal";
+import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/initial-profile";
+import { redirect } from "next/navigation";
+import { FC } from "react";
+
+interface SetupPageProps {
+  // Define prop types here
+}
+
+const SetupPage: FC<SetupPageProps> = async () => {
+  const profile = await initialProfile();
+
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+
+  return <InitialModal />
+};
+
+export default SetupPage;
