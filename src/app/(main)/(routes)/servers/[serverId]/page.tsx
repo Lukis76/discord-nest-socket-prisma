74c7bf9d4cf8fb0a50cb82a86dbe09fb7094d0e4
Lucas Manoguerra <kiu.manoguerra@gmail.com>
2023-09-03 19:56:13 -0300

    .

---
+++ src/app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react';
+
+interface ServerIdPageProps {
+  // Define prop types here
+}
+
+const ServerIdPage: FC<ServerIdPageProps> = () => {
+  return (
+    <div>
+      <h1>ServerId</h1>
+    </div>
+  );
+};
+
+export default ServerIdPage;
---
+++ src/app/(main)/(routes)/servers/page.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react';
+
+interface ServersPageProps {
+  // Define prop types here
+}
+
+const ServersPage: FC<ServersPageProps> = () => {
+  return (
+    <div>
+      <h1>Servers</h1>
+    </div>
+  );
+};
+
+export default ServersPage;
